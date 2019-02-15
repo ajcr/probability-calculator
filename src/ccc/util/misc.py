d139741d5b69e2382b89b7d71836f2ab35e04338
ajcr <ajcriley@gmail.com>
2019-02-15 22:06:17 +0000

    Improve docstrings and paramter names

--- src/ccc/util/misc.py
+++ src/ccc/util/misc.py
@@ -1,13 +1,18 @@
 from itertools import chain, combinations
-from typing import List, Sequence, Generator, TypeVar
+from typing import List, Sequence, Generator, TypeVar, Tuple
 
 T = TypeVar('T')
 
-def subsets(x: Sequence[List[T]]) -> Generator[List[T], None, None]:
+def subsets(seq: Sequence[Sequence[T]]) -> Generator[Tuple[int, List[T]], None, None]:
     """
+    Generate all non-empty subsets of a sequence of sequences,
+    merged into a single sequence.
+
+    Each merged sequence is returned, along with the number of
+    subsets that were merged.
 
     """
-    for n in range(1, len(x) + 1):
-        combs = combinations(x, n)
+    for n in range(1, len(seq) + 1):
+        combs = combinations(seq, n)
         for cmb in combs:
             yield n, list(chain.from_iterable(cmb))
